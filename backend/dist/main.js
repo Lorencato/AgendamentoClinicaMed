6b816d36033ac1d31a539524b1870450d85810bf
Lorencato <lorenrafa2015@gmail.com>
2026-06-02 12:15:24 -0300

    pequena atualização

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -8,7 +8,7 @@ async function bootstrap() {
     app.enableCors({
         origin: 'http://localhost:5173',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-        allowedHeaders: ['Content-Type'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
     });
     app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true, transform: true }));
     app.setGlobalPrefix('api');
